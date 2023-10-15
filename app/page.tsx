e1dbfe1a5a29eefb30096deb50d46a0ee4e06139
Michael Chan <mijoch@gmail.com>
2023-10-15 10:44:17 -0700

    add confetti!

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import * as React from 'react'
+import JSConfetti from 'js-confetti'
 
 type Manner = 'solo' | 'partner' | 'unknown' | null
 
@@ -8,6 +9,28 @@ let initialEvacuations: Manner[] = Array(19).fill(null)
 
 export default function Home() {
   let [evacuations, updateEvacuations] = React.useState(initialEvacuations)
+
+  React.useEffect(() => {
+    if (evacuationsRemaining(evacuations) > 1) return
+
+    const jsConfetti = new JSConfetti()
+
+    jsConfetti.addConfetti({
+      confettiColors: [
+        '#ff0a54',
+        '#ff477e',
+        '#ff7096',
+        '#ff85a1',
+        '#fbb1bd',
+        '#f9bec7',
+      ],
+      confettiRadius: 6,
+    })
+
+    return () => {
+      jsConfetti.clearCanvas()
+    }
+  }, [evacuations])
 
   function addNextEvacuation(manner: Manner) {
     let nextEvacuation = evacuations.findIndex((element) => element === null)
